d6dc708ada8296789632f7d19e1ac6965fae5909
Vignesh Mohankumar <vignesh@ccs.neu.edu>
2016-02-27 14:33:01 -0500

    Update results to use indices

--- 7/feeding/result.py
+++ 7/feeding/result.py
@@ -1,11 +1,27 @@
 from collections import namedtuple
 
 """
+a JSONFeedingResult is one of:
+    - JSONFatTissueResult
+    - JSONVegetarianResult
+    - JSONCarnivoreResult
+    - JSONNoFeedingResult
+
+ SpeciesIndex is a Natural representing the index of a species
+ PlayerIndex is a Natural representing the index of a player
+ FoodTokens is a Nat representing a number of food tokens
+
+ JSONFatTissueResult is a [SpeciesIndex, FoodTokens]
+ JSONVegetarianResult is a SpeciesIndex
+ JSONCarnivoreResult is a [SpeciesIndex, PlayerIndex, SpeciesIndex]
+ JSONNoFeedingResult is false
+
 a FeedingResult is one of:
     - FatTissueResult
     - VegetarianResult
     - CarnivoreResult
-    - None (indicates player does not wish to feed)
+    - NoFeedingResult
+    - None (if feeding is not possible)
 """
 
 
@@ -13,66 +29,63 @@ class FatTissueResult(
         namedtuple('FatTissueResult', ['species', 'num_tokens'])):
     """implements a FatTissueResult
 
-    :attr species: species
-    :type species: Species
+    :attr species: species index
+    :type species: int
 
     :attr num_tokens: number of tokens requested
     :type num_tokens: int
     """
 
     def __eq__(self, other):
-        return (isinstance(other, self.__class__) and
-                self.species == other.species and
+        return (self.species == other.species and
                 self.num_tokens == other.num_tokens)
 
     def to_json(self):
         """creates a JSON representation of the result
 
         :returns: [species, num tokens asked for]
-        :rtype: [Species+, Nat]
+        :rtype: JSONFatTissueResult
         """
 
-        return [self.species.to_json(), self.num_tokens]
+        return [self.species, self.num_tokens]
 
 
 class VegetarianResult(namedtuple('VegetarianResult', ['species'])):
     """implements a VegetarianResult
 
-    :attr species: species
-    :type species: Species
+    :attr species: species index
+    :type species: int
     """
 
     def __eq__(self, other):
-        return (isinstance(other, self.__class__) and
-                self.species == other.species)
+        return self.species == other.species
 
     def to_json(self):
         """creates a JSON representation of the result
 
         :returns: [species]
-        :rtype: [Species+]
+        :rtype: JSONVegetarianResult
         """
 
-        return [self.species.to_json()]
+        return [self.species]
 
 
 class CarnivoreResult(
         namedtuple('CarnivoreResult', ['species', 'player', 'to_attack'])):
     """implements a CarnivoreResult
 
-    :attr species: species attacking
-    :type species: Species
+    :attr species: species attacking index
+    :type species: int
 
-    :attr player: player to attack
-    :type player: BasePlayer
+    :attr player: player to attack index
+    :type player: int
 
-    :attr to_attack: species defending
-    :type to_attack: Species
+    :attr to_attack: species defending index
+    :type to_attack: int
     """
 
     def __eq__(self, other):
-        return (isinstance(other, self.__class__) and
-                self.species == other.species and
+        return (self.species == other.species and
                 self.player == other.player and
                 self.to_attack == other.to_attack)
 
@@ -80,11 +93,10 @@ class CarnivoreResult(
         """creates a JSON representation of the result
 
         :returns: [attacker species, player to attack, species to attack]
-        :rtype: [Species+, Player, Species+]
+        :rtype: JSONCarnivoreResult
         """
 
-        return [self.species.to_json(), self.player.to_json(),
-                self.to_attack.to_json()]
+        return [self.species, self.player, self.to_attack]
 
 
 class NoFeedingResult:
@@ -94,7 +106,7 @@ class NoFeedingResult:
         """creates a JSON representation of the result
 
         :returns: False
-        :rtype: bool
+        :rtype: JSONNoFeedingResult
         """
 
         return False
